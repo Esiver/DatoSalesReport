3341ac143fbb2e8a5f7e7b9a4c9d0b83553b5d44
EmilNS <emilarme@gmail.com>
2020-10-11 23:04:54 +0800

    More functions

--- cvsreader.py
+++ cvsreader.py
@@ -2,45 +2,37 @@ import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import datetime
-print('........')
-sales_data = pd.read_csv(r'clean_data.csv',  sep='\t')#, sep='\t'
-print('--------')
-print(sales_data['date'])
-print('--------')
-print(sales_data.columns)
-print('--------')
-print('--------')
-#sales_data.columns['date','units','sales','spend','Acos','CPC']
-print(sales_data.columns)
-sales_data['date'] = pd.to_datetime(sales_data['date'], errors='coerce') #make a 'date'-column in datetime format
-print('xxxxxxxx')
+
 import weekgroup, daygroup, pdfMaker#, write_html, dftopdf
 
-#def main(datafile):
-#    week = weekgroup.main(datafile) #Data by week
-day = daygroup.main(sales_data) #day data
+def getSalesData(file):
+    sales_data = pd.read_csv(file, sep='\t')#, sep='\t'
+    try:
+        sales_data['date'] = pd.to_datetime(sales_data['date'], errors='coerce') #make a 'date'-column in datetime format
+    except:
+        print('Error found, trying different seperator.')
+        sales_data = pd.read_csv(file, sep=',', engine='c')#, sep='\t'
+        sales_data['date'] = pd.to_datetime(sales_data['date'], errors='coerce') #make a 'date'-column in datetime format
+    
+    return sales_data 
 
-def week(datafile):
+#sales_data.columns['date','units','sales','spend','Acos','CPC']
+
+
+def weekData(datafile):
+    getSalesData(datafile)
     week = weekgroup.main(datafile) #Data by week
     week_stats = weekgroup.stats(week) #dive into the stats for week    
     weekgroup.makeCsv(week) #make csv file with week dataframe
     weekgroup.plot(week, week_stats) #make plot, saves a file but doesn't plt.plot()
     #weekgroup.makeReport() #make a report in .pdf
 
-week(sales_data)
-#main(sales_data)
+def dayData(datafile):
+    day = daygroup.main(datafile) #day data
+    day_stats = daygroup.stats(day) #day statistics
+    daygroup.makeCsv(day) #csv for day dataframe
+    daygroup.plot(day, day_stats) #make plot for days(-data)
 
-#week_stats = weekgroup.stats(week) #dive into the stats for week
-day_stats = daygroup.stats(day) #... and for days
-
-#weekgroup.makeCsv(week) #make csv file with week dataframe
-daygroup.makeCsv(day) #csv for day dataframe
-
-#weekgroup.plot(week, week_stats) #make plot, saves a file but doesn't plt.plot()
-daygroup.plot(day, day_stats) #make plot for days(-data)
-
-#weekgroup.makeReport() #make a report in .pdf
-daygroup.makeReport() #report in .pdf
 
 def formatMain(mainFile):
     csv_df = mainFile.to_csv()
@@ -48,7 +40,7 @@ def formatMain(mainFile):
     print('formatted main data')
     return csv_df
 
-
+sales_data = getSalesData('clean_data.csv')
 formatMain(sales_data) #format my original data into a dataframe friendly csv
 print(formatMain(sales_data))
 files_to_read = ['week_max5.csv','week_min5.csv','all_data.csv']
